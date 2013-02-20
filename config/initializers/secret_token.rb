3ae024b869aa66608601d9127952963bd436d2dc
Nishant Samel <nishants@weboniselab.com>
2013-02-20 18:17:07 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotMysql::Application.config.secret_token = 'd62d846651172d82e11d24547cbf5ba13da6a54b68877d112682439ec0258602acea898996b994c9adacb7179ee357cd61354bc4d9cc8b53de408b61e1e0fe2a'
